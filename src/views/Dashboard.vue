c237f26d582e44956e3caf18b078e122209fd258
Pampah91 <zerbib.corentin@gmail.com>
2021-03-21 19:24:34 +0100

    added route to dashboard vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboard">
+    <h1>Mon tableau de bord</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import UserForm from '@/components/UserForm.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    
+  }
+}
+</script>
